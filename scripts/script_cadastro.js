c09ab4c52f8279b333e207b91aa74e50be636c4b
Vinicius Nunes <nissinho_mentiara@hotmail.com>
2023-11-30 20:19:36 -0300

    fizemos o link dos arquivos html entre si através dos botões e o acrescimo singelo do Javascript

---
+++ scripts/script_cadastro.js
@@ -0,0 +1,6 @@
+const bot_cadastro = document.querySelector('.cadastroenc');
+bot_cadastro.addEventListener('click' , function(){
+  alert("Cadastro feito, por gentileza faça login!")
+
+} );
+
---
+++ scripts/script_login.js
@@ -0,0 +1,5 @@
+const bot_enviar = document.querySelector('.logger');
+bot_enviar.addEventListener('click' , function(){
+  alert("Login realizado com sucesso, bem vindo ao portal!")
+
+} );
