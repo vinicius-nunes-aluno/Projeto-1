1892c9e342e3c20d50617d02514b98330c700948
vinicius-nunes-aluno <152037450+vinicius-nunes-aluno@users.noreply.github.com>
2025-09-28 18:50:32 -0300

    Acerto na porta da publicação

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const URI = 'mongodb+srv://admin:JuqOZAX9QrBInXgE@cluster0.zashxsd.mongodb.net/'
+const URI = 'mongodb+srv://nissinhomentiara:MXIt4RK6tgwfyE1u@cluster2.8gdoscg.mongodb.net/'
 
 const databaseConnection = async () => {
     if (!global.mongoose) {
